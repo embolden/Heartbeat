e2ab33482e62729a670addc983d57d7fbaa3c5aa
Matt <matthewkeehner@gmail.com>
2020-05-01 22:38:32 -0500

    Change the starting health percentage to be 45% instead of 20%.  This is actually closer to the Mod Requester's original idea.

--- Heartbeat/models/ModConfig.cs
+++ Heartbeat/models/ModConfig.cs
@@ -7,6 +7,6 @@ namespace Heartbeat
     class ModConfig
     {
         public bool HeartBeatEnabled { get; set; } = true;
-        public float HeartBeatAlertPercent { get; set; } = 20;
+        public float HeartBeatAlertPercent { get; set; } = 45.0F;
     }
 }
